48572d658dac0cf25d7bee0ac3a2f84d0c210812
BellRazvan <razvan_bell@yahoo.com>
2025-03-25 22:19:04 +0200

    Initialize frontend and backend

---
+++ timemanager-service/src/main/java/com/time/timemanager/TimemanagerApplication.java
@@ -0,0 +1,11 @@
+package com.time.timemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimemanagerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TimemanagerApplication.class, args);
+	}
+}
---
+++ timemanager-service/src/test/java/com/time/timemanager/TimemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.time.timemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
